bdc2079f3af411306911706e4f2131aad79b0021
wangbin <13331036973@163.com>
2020-02-08 17:23:49 +0800

    20200208

---
+++ src/JsonServer.js
@@ -0,0 +1,28 @@
+// json-server --watch --port 18080 ./src/assets/db.json
+// pm2 start server.js --name app
+// json-server --watch -c jserver.json db.json
+
+// server.js
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const path = require('path');
+const router = jsonServer.router(path.resolve(__dirname, './Assets/db.json'))
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares.concat);
+server.use(router);
+
+
+//自定义用户校验
+/*
+server.use((req, res, next) => {
+    if (isAuthorized(req)) { // add your authorization logic here
+        next() // continue to JSON Server router
+    } else {
+        res.sendStatus(401)
+    }
+})
+*/
+server.listen(3000, () => {
+    console.log('JSON Server is running.')
+})
